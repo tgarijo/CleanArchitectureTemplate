52269020aa65a719d0469bf6325c71cccd06b794
Tomas Garijo <tgarijo@clickonline.es>
2026-06-26 14:52:34 +0200

    Instalación y Configuración de Extensiones

---
+++ CleanArchitectureTemplate.Domain/Entities/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArchitectureTemplate.PresentationWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

d9041782bcd3d383d872b7aee39f4cfc9f1f5cb8
Tomas Garijo <tgarijo@clickonline.es>
2026-06-29 16:31:59 +0200

    add productDTO

---
+++ CleanArchitectureTemplate.Application/DTOs/ProductoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Application.DTOs;
+
+public class ProductoDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage= "Product name required")]
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+}

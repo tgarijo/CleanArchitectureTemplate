025e204029979975eb2531ceea0c568c03bebbad
Tomas Garijo <tgarijo@clickonline.es>
2026-06-29 16:59:19 +0200

    add productService

--- CleanArchitectureTemplate.Application/DTOs/ProductDTO.cs
+++ CleanArchitectureTemplate.Application/DTOs/ProductDTO.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 
 namespace CleanArchitectureTemplate.Application.DTOs;
 
-public class ProductoDTO
+public class ProductDTO
 {
     public int Id { get; set; }
 
     [Required(ErrorMessage= "Product name required")]
+    [StringLength(100, ErrorMessage ="Product name exceed 100 characters")]
     public string Name { get; set; }
 
+    [Range(0.01, 10000.00, ErrorMessage = "The price must 0.01 to 10000.00 range")]
     public decimal Price { get; set; }
     public string Description { get; set; }
 
---
+++ CleanArchitectureTemplate.Application/UseCases/ProductService.cs
@@ -0,0 +1,116 @@
+﻿using CleanArchitectureTemplate.Application.DTOs;
+using CleanArchitectureTemplate.Domain.Entities;
+using CleanArchitectureTemplate.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Application.UseCases;
+
+public class ProductService 
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task AddSync(ProductDTO productDTO)
+    {
+        if(string.IsNullOrEmpty(productDTO.Name)) {
+            throw new ArgumentException("Product name cannot be null");
+        }
+
+        if (productDTO.Price < 0)
+        {
+            throw new ArgumentException("Product price cannot be negative");
+        }
+     
+        var product = new Product
+        {
+            Name = productDTO.Name,
+            Price = productDTO.Price,
+            Description = productDTO.Description,
+            CreationDate = DateTime.UtcNow
+        };
+
+        await _productRepository.AddSync(product);
+        
+    }
+ 
+
+    public async Task DeleteAsync(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+
+        if (product == null)
+        {
+            throw new KeyNotFoundException($"Product with ID {id} not found.");
+        }
+
+        await _productRepository.DeleteAsync(id);
+    }
+
+    public async Task<List<ProductDTO>> GetAllAsync()
+    {
+        var products = await _productRepository.GetAllAsync();
+
+        return products.Select(product => new ProductDTO
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Description = product.Description, 
+            CreationDate = product.CreationDate
+        }).ToList();
+        
+
+    }
+   
+
+    public async Task<ProductDTO> GetByIdAsync(int id)
+    {
+        var producto = await _productRepository.GetByIdAsync(id);
+
+        if (producto == null)
+        {
+            throw new KeyNotFoundException($"Product with ID {id} not found.");
+        }
+
+        return new ProductDTO
+        {
+            Id = producto.Id,
+            Name = producto.Name,
+            Price = producto.Price,
+            Description = producto.Description,
+            CreationDate = producto.CreationDate
+        };  
+    }
+
+    public Task<Product> GetByNameAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(ProductDTO productDTO)
+    {
+        if (productDTO == null)
+        {
+            new ArgumentNullException(nameof(productDTO), "ProductDTO cannot be null.");
+        }
+
+        var product = new Product
+        {
+            Id = productDTO.Id,
+            Name = productDTO.Name,
+            Price = productDTO.Price,
+            Description = productDTO.Description,
+            CreationDate = productDTO.CreationDate
+        };
+
+        await _productRepository.UpdateAsync(product);  
+    }
+}
